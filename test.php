30cfba29b55bc18ddf706d449bff5a067b8387ca
Zeeshan Shaikh <zeeshan@Zeeshans-MacBook-Pro.local>
2024-07-29 18:56:00 +0530

    generating document

--- test.php
+++ test.php
@@ -1,13 +1,9 @@
 <?php 
 require_once 'PHPWord/src/PhpWord/Autoloader.php'; // Adjust the path as needed
+\PhpOffice\PhpWord\Autoloader::register();
 
-use PhpOffice\PhpWord\PhpWord;
-use PhpOffice\PhpWord\IOFactory;
-
-// Create a new PHPWord object
-$phpWord = new PhpWord();
-
-
+// Creating the new document...
+$phpWord = new \PhpOffice\PhpWord\PhpWord();
 
 // Output success message
 echo "Document created successfully: $filename";
